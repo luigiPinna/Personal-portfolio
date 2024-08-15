fee83f2a75ca4ea94e98b97b7ef94cc33bf6007c
Luigi Pinna <luigipinna3@gmail.com>
2024-08-15 16:50:27 +0200

    some fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
-const path = require('path');
-
 module.exports = {
-  webpack: (config) => {
-    config.resolve.modules.push(path.resolve('./src'));
-    return config;
-  },
-  experimental: {
-    appDir: true, // O false, se stai usando pages invece di app
-  },
-};
+    compiler: {
+      styledComponents: true
+    }
+  }
