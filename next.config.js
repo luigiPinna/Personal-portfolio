00e347c95defbb8c0d7eb4ac221ba4b3b15f2f6f
luigiPinna <luigipinna3@gmail.com>
2026-05-18 19:42:55 +0200

    next config update

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
-const isProd = process.env.NODE_ENV === 'production';
-const repo = 'Personal-portfolio';
-const basePath = isProd ? `/${repo}` : '';
-
 module.exports = {
   compiler: {
     styledComponents: true,
   },
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,
-  },
-  basePath,
-  assetPrefix: isProd ? `/${repo}/` : '',
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
   },
 };
